96dbe8a61d0791d0b0053d6ff3bd327b24918efb
mosfiqurRahmanJsd <nisho.mushfiq2020@gmail.com>
2025-11-21 10:58:04 +0600

    fetch user data

--- fetch.js
+++ fetch.js
@@ -1,5 +1,5 @@
 function handleLoadData () {
-    fetch('https://jsonplaceholder.typicode.com/posts')
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
     .then(res => res.json()) 
     .then(data => console.log(data)); 
 }
---
+++ moreapi.js
@@ -0,0 +1,11 @@
+
+const handleLoadUser = () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json()) 
+    .then(data => displayUser(data))
+}
+
+
+const displayUser = (users) => {
+    console.log(users); 
+}
