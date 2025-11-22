afd2a1369651db7245e335731fb7899af0a34b3d
mosfiqurRahmanJsd <nisho.mushfiq2020@gmail.com>
2025-11-22 11:13:01 +0600

    event loop

---
+++ eventLoop.js
@@ -0,0 +1,26 @@
+function a() {
+    b(); 
+    console.log("a"); 
+}
+function b() {
+    c(); 
+    console.log("b"); 
+}
+
+setTimeout(() => {
+    console.log('timeout')
+}, 1000)
+
+function c() {
+    d()
+    console.log("c"); 
+}
+function d() {
+    e()
+    console.log("d"); 
+}
+function e() {
+    console.log("e"); 
+}
+
+a(); 
